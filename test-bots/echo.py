0a16631023b7185d26f3f96b4ad7b97b83cce29b
Ali Jafari <mmdddpdd@gmail.com>
2025-09-08 22:07:22 +0330

    Create echo.py

---
+++ test-bots/echo.py
@@ -0,0 +1,11 @@
+from bale import Bot
+
+TOKEN = "YOUR_BOT_TOKEN"
+bot = Bot(TOKEN)
+
+async def handle(msg):
+    if msg.text:
+        await bot.client.send_message(msg.chat["id"], f"تو گفتی: {msg.text}")
+
+bot.add_message_handler(handle)
+bot.run()
