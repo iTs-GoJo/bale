6cc77508c6f37e46eeb1411bbf83d96e39ea29ca
Ali Jafari <mmdddpdd@gmail.com>
2025-09-08 22:03:45 +0330

    Create welcome.py

---
+++ test-bots/welcome.py
@@ -0,0 +1,11 @@
+from bale import Bot
+
+TOKEN = "YOUR_BOT_TOKEN"
+bot = Bot(TOKEN)
+
+async def handle(msg):
+    if msg.text == "/start":
+        await bot.client.send_message(msg.chat["id"], "سلام 😎 خوش اومدی به ربات من!")
+
+bot.add_message_handler(handle)
+bot.run()
