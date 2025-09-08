bd0fed7c16b1f7c8184b100dbd8a513a389cb445
Ali Jafari <mmdddpdd@gmail.com>
2025-09-08 22:05:15 +0330

    Create send-img.py

---
+++ test-bots/send-img.py
@@ -0,0 +1,12 @@
+from bale import Bot
+
+TOKEN = "YOUR_BOT_TOKEN"
+bot = Bot(TOKEN)
+
+async def handle(msg):
+    if msg.text == "/photo":
+        url = "https://picsum.photos/300/200"  # عکس رندوم
+        await bot.client.send_photo(msg.chat["id"], url, "یه عکس رندوم 📸")
+
+bot.add_message_handler(handle)
+bot.run()
